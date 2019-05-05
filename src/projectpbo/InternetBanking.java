79d0ad2f6347adbe34586ed69ab6292708b49733
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-05-06 00:17:17 +0700

    make start from center

--- src/projectpbo/InternetBanking.java
+++ src/projectpbo/InternetBanking.java
@@ -16,6 +16,7 @@ public class InternetBanking extends javax.swing.JFrame {
      */
     public InternetBanking() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
--- src/projectpbo/Login.java
+++ src/projectpbo/Login.java
@@ -26,6 +26,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         db = new Database();
+        setLocationRelativeTo(null);
         
     }
 
